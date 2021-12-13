301fcf5918ba25c23a0bbdff8326de1d781b9728
Viahduc <82482523+Viahduc@users.noreply.github.com>
2021-12-12 22:09:10 -0500

    Add files via upload

--- viahduc-essentiels.sh
+++ viahduc-essentiels.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-pacman -S vim steam discord libreoffice-still alacritty firefox ark neofetch sl gimp pcmanfm clementine thunderbird lutris keepassxc vlc htop
-echo Installed program: vim, steam, discord, libreoffice-still, alacritty, firefox, ark, sl, gimp, pcmanfm, clementine, thunderbird, lutris, keepassxc, vlc, htop and neofetch
-	
+
+#pacman
+sudo pacman -S vim steam discord libreoffice-still alacritty firefox ark neofetch sl gimp pcmanfm clementine thunderbird lutris keepassxc vlc htop
+
+#git
+
+#pfetch
+git clone https://github.com/dylanaraps/pfetch
+cd pfetch
+sudo make install
+cd ../
+rm -rf pfetch
+
+#list
+echo Installed program with pacman: vim, steam, discord, libreoffice-still, alacritty, firefox, ark, sl, gimp, pcmanfm, clementine, thunderbird, lutris, keepassxc, vlc, htop and neofetch
+echo Installed program with git: pfetch
