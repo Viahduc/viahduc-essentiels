a2942391234253ec3952b5f71c8db80b2eee358c
Viahduc <82482523+Viahduc@users.noreply.github.com>
2021-12-12 21:42:36 -0500

    Add files via upload

--- viahduc-essentiels.sh
+++ viahduc-essentiels.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-pacman -S vim steam discord libreoffice-still alacritty firefox ark neofetch sl
-echo Installed program: vim, steam, discord, libreoffice-still, alacritty, firefox, ark, sl and neofetch
+pacman -S vim steam discord libreoffice-still alacritty firefox ark neofetch sl gimp pcmanfm clementine thunderbird lutris keepassxc vlc htop
+echo Installed program: vim, steam, discord, libreoffice-still, alacritty, firefox, ark, sl, gimp, pcmanfm, clementine, thunderbird, lutris, keepassxc, vlc, htop and neofetch
+	
